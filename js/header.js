563f562aa27f8b82f1a6305ade4e0a01671f4b28
Mohamad Salimi <mohamad.salimi@hotmail.com>
2022-02-09 01:47:13 +0200

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,9 +1,13 @@
-function createHeader(){
+let path = window.location.pathname;
+let page = path.split("/").pop();
+console.log( page );
+
+function createHeaderIndex(){
     let header = `
             <div class="container">
                 <div class="top-header">
                     <div class="logo">
-                        <img src="images/sbc-logo.png">
+                        <img src="../images/sbc-logo.png">
                     </div>
                     <ul class="navbar">
                         <li><a href="#">الخدمات الالكترونية</a></li>
@@ -14,12 +18,100 @@ function createHeader(){
                     </ul>
                 </div>
                 <div class="header-text">
-                    <h2>الأدلة الارشادية</h2>
-                    <p>المركز السعودي للأعمال / منصة الأعمال / الأدلة الارشادية</p>
+                <h2>الأدلة الارشادية</h2>
+                <p>المركز السعودي للأعمال / منصة الأعمال / الأدلة الارشادية</p>
                 </div>
                 
             </div>
     `;
+    
     document.querySelector('.header').innerHTML = header;
 }
-createHeader();
+
+function createHeaderSector(){
+    let header = `
+            <div class="container">
+                <div class="top-header">
+                    <div class="logo">
+                        <img src="../images/sbc-logo.png">
+                    </div>
+                    <ul class="navbar">
+                        <li><a href="#">الخدمات الالكترونية</a></li>
+                        <li><a href="#">تواصل معنا</a></li>
+                        <li><a href="#">AR</a></li>
+                        <li><a href="#"><i class="fas fa-search"></i></a></li>
+                        <li><a href="#"><i class="fas fa-user"></i></a></li>
+                    </ul>
+                </div>
+                <div class="header-text">
+                <h2>إصدار ترخيص مرافق الإيواء السياحي</h2>
+                <p>المركز السعودي للأعمال / منصة الأعمال / قطاع السياحة</p>
+                </div>
+                
+            </div>
+    `;
+    
+    document.querySelector('.header').innerHTML = header;
+}
+
+function createHeaderIwaa(){
+    let header = `
+            <div class="container">
+                <div class="top-header">
+                    <div class="logo">
+                        <img src="../images/sbc-logo.png">
+                    </div>
+                    <ul class="navbar">
+                        <li><a href="#">الخدمات الالكترونية</a></li>
+                        <li><a href="#">تواصل معنا</a></li>
+                        <li><a href="#">AR</a></li>
+                        <li><a href="#"><i class="fas fa-search"></i></a></li>
+                        <li><a href="#"><i class="fas fa-user"></i></a></li>
+                    </ul>
+                </div>
+                <div class="header-text">
+                <h2>قطاع السياحة</h2>
+                <p>المركز السعودي للأعمال / منصة الأعمال / قطاع السياحة</p>
+                </div>
+                
+            </div>
+    `;
+    
+    document.querySelector('.header').innerHTML = header;
+}
+
+function createHeaderAn(){
+    let header = `
+            <div class="container">
+                <div class="top-header">
+                    <div class="logo">
+                        <img src="../images/sbc-logo.png">
+                    </div>
+                    <ul class="navbar">
+                        <li><a href="#">الخدمات الالكترونية</a></li>
+                        <li><a href="#">تواصل معنا</a></li>
+                        <li><a href="#">AR</a></li>
+                        <li><a href="#"><i class="fas fa-search"></i></a></li>
+                        <li><a href="#"><i class="fas fa-user"></i></a></li>
+                    </ul>
+                </div>
+                <div class="header-text">
+                <h2>الأنشطة السياحية</h2>
+                <p>المركز السعودي للأعمال / منصة الأعمال / قطاع السياحة</p>
+                </div>
+                
+            </div>
+    `;
+    
+    document.querySelector('.header').innerHTML = header;
+}
+
+if(page === "index.html"){
+    createHeaderIndex();
+}else if(page === "sector.html"){
+    createHeaderSector();
+}else if(page === "iwaainfo.html"){
+    createHeaderIwaa();
+}else {
+    createHeaderAn();
+}
